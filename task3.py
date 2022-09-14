f3cfd75ee54fd9ad1c94b37df39278337e0dfe6b
Volodymyr Voloshchuk <81922146+medmas@users.noreply.github.com>
2022-09-14 21:53:12 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,18 @@
+#Task 1
+
+b = 'Volodymyr'
+c = 'Wednesday'
+print(f'Good day {b}! {c} is a perfect day to learn some python.')
+
+#Task 2
+
+first_name = 'Volodymyr '
+second_name = 'Voloshchuk'
+print(f'Hello! {first_name}{second_name}')
+
+#Task 3
+
+a = 6
+b = 3
+
+print(a + b, a - b, a / b, a * b, a ** b, a % b, a // b, sep='\n')
