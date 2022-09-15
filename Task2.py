99a3a17b70227f2e2e42cfbd36413f73ca9669aa
Volodymyr Voloshchuk <81922146+medmas@users.noreply.github.com>
2022-09-15 22:45:03 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,7 +1,10 @@
-a = '#########'
-b = '\n#       #'
-c = '\n' + a
+#Download colorama pakage first
+from colorama import Fore
+
+a = Fore.LIGHTBLUE_EX + '#########'
+b = Fore.LIGHTYELLOW_EX + '\n#       #'
+c ='\n' + a
 d = '\n'
 
-print(a, b*3, c, d, b*2, c, b*2)
+print(a, b*3, c, d, b*2, c, b*2, '\a')
 
